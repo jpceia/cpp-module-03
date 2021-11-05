f6ea387c910ec6e2679ea6ce44aeafdf459aa4de
jpceia <joao.p.ceia@gmail.com>
2021-11-05 00:25:05 +0000

    ex00

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 19:18:47 by jceia             #+#    #+#             */
-/*   Updated: 2021/11/04 19:25:27 by jceia            ###   ########.fr       */
+/*   Updated: 2021/11/05 00:14:35 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 int main(void)
 {
-	ClapTrap	p("Pikachu");
+    ClapTrap    p("Pikachu");
 
-	p.attack("Team Rocket");
-	p.takeDamage(5);
-	p.attack("Team Rocket");
-	p.takeDamage(5);
-	p.attack("Team Rocket");
-	p.beRepaired(100);
-	p.beRepaired(100);
-	return 0;
+    p.attack("Team Rocket");
+    p.takeDamage(5);
+    p.attack("Team Rocket");
+    p.takeDamage(5);
+    p.attack("Team Rocket");
+    p.beRepaired(100);
+    p.beRepaired(100);
+    return 0;
 }
